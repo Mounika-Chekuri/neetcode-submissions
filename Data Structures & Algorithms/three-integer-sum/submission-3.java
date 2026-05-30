c196e2a9461a52611b9674e7a58e265b6bfdf6ce
Mounika-Chekuri <43726925+Mounika-Chekuri@users.noreply.github.com>
2026-05-30 12:55:14 -0500

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.java
@@ -0,0 +1,47 @@
+// nums = [-1,0,1,2,-1,4]
+// O/p->  [[-1,-1,2],[-1,0,1]]
+
+//Arrays.sort => [-1,-1,0,1,2,4]
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        int n  = nums.length;
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        Set<List<Integer>> s = new HashSet<>();
+        Arrays.sort(nums);
+        for(int i=0;i<n;i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int target = nums[i]*-1;
+            int low = i+1;
+            int high = n-1;
+            while(low<high)
+            {
+            
+                if(nums[low]+nums[high]==target)
+                {
+                  List<Integer> triplet = new ArrayList<>();
+                  triplet.add(nums[i]);
+                   triplet.add(nums[low]);
+                    triplet.add(nums[high]);
+                    res.add(triplet);
+                    low++;
+                    high--;
+                    while (low < high && nums[low] == nums[low - 1]) low++;
+                   
+                   
+                     
+                }
+               else  if(nums[low]+nums[high]<target)
+               {
+                low++;
+               }
+               else high--;
+            }
+
+        }
+        return res;
+        
+    }
+}
